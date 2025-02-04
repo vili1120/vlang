24c69ad5afbe0520f5b5e7f7d46613440a2451be
vili1120 <vili1120@localhost.my.domain>
2025-02-04 16:09:44 +0100

    Added power operation - Now the program can perform power operations using the '**' symbols by vili1120

--- vlang.py
+++ vlang.py
@@ -81,6 +81,7 @@ TT_PLUS = "PLUS"
 TT_MINUS = "MINUS"
 TT_MUL = "MUL"
 TT_DIV = "DIV"
+TT_POW = "POW"
 TT_LPAREN = "LPAREN"
 TT_RPAREN = "RPAREN"
 TT_EOF = "EOF"
@@ -130,8 +131,9 @@ class Lexer:
                 tokens.append(Token(TT_MINUS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == "*":
-                tokens.append(Token(TT_MUL, pos_start=self.pos))
-                self.advance()
+                tokens.append(self.make_pow())
+                # tokens.append(Token(TT_MUL, pos_start=self.pos))
+                # self.advance()
             elif self.current_char == "/":
                 tokens.append(Token(TT_DIV, pos_start=self.pos))
                 self.advance()
@@ -149,6 +151,23 @@ class Lexer:
         
         tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
+    
+    def make_pow(self):
+        pos_start = self.pos.copy()
+        mul_count = 0
+        
+        while self.current_char != None and self.current_char == "*":
+            if self.current_char == "*":
+                if mul_count == 2: break
+                mul_count += 1
+            else:
+                break
+            self.advance()
+
+        if mul_count == 1:
+            return Token(TT_MUL, pos_start=pos_start, pos_end=self.pos)
+        elif mul_count == 2:
+            return Token(TT_POW, pos_start=pos_start, pos_end=self.pos)
 
     def make_number(self):
         num_str = ""
@@ -244,21 +263,15 @@ class Parser:
         if not res.error and self.current_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected '+', '-', '*', '/'"
+                "Expected '+', '-', '*', '/', '**'"
             ))
         return res
-
-    def factor(self):
+    
+    def atom(self):
         res = ParseResult()
         tok = self.current_tok
 
-        if tok.type in (TT_PLUS, TT_MINUS):
-            res.register(self.advance())
-            factor = res.register(self.factor())
-            if res.error: return res
-            return res.success(UnaryOpNode(tok, factor))
-
-        elif tok.type in (TT_INT, TT_FLOAT):
+        if tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
 
@@ -277,8 +290,23 @@ class Parser:
 
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
-            "Expected int or float"
+            "Expected int, float, '+', '-', '('"
         ))
+
+    def power(self):
+        return self.bin_op(self.atom, (TT_POW, ), self.factor)
+
+    def factor(self):
+        res = ParseResult()
+        tok = self.current_tok
+
+        if tok.type in (TT_PLUS, TT_MINUS):
+            res.register(self.advance())
+            factor = res.register(self.factor())
+            if res.error: return res
+            return res.success(UnaryOpNode(tok, factor))
+
+        return self.power()
 
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
@@ -286,15 +314,17 @@ class Parser:
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
         res = ParseResult()
-        left = res.register(func())
+        left = res.register(func_a())
         if res.error: return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
             res.register(self.advance())
-            right = res.register(func())
+            right = res.register(func_b())
             if res.error: return res
             left = BinOpNode(left, op_tok, right)
 
@@ -358,6 +388,10 @@ class Number:
                 )
             return Number(self.value / other.value).set_context(self.context), None
 
+    def __pow__(self, other):
+        if isinstance(other, Number):
+            return Number(self.value ** other.value).set_context(self.context), None
+
     def __repr__(self):
         return str(self.value)
 
@@ -398,6 +432,8 @@ class Interpreter:
             result, error = left.__mul__(right)
         elif node.op_tok.type == TT_DIV:
             result, error = left.__div__(right)
+        elif node.op_tok.type == TT_POW:
+            result, error = left.__pow__(right)
 
         if error:
             return res.failure(error)
