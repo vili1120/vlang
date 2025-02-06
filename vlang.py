f310b0a78b9fd2dfef26dd9205c05751768a42ab
vili1120 <vili1120@localhost.my.domain>
2025-02-06 21:16:22 +0100

    Working if statement - Syntax:     if comparison { expression } by vili1120

--- vlang.py
+++ vlang.py
@@ -113,7 +113,11 @@ TT_EOF = "EOF"
 KEYWORDS = [
     "and",
     "or",
-    "not"
+    "not",
+
+    "if",
+    "elif",
+    "else"
 ]
 
 class Token:
@@ -345,6 +349,14 @@ class UnaryOpNode:
     def __repr__(self):
         return f"({self.op_tok}, {self.node})"
 
+class IfNode:
+    def __init__(self, cases, else_case):
+        self.cases = cases
+        self.else_case = else_case
+
+        self.pos_start = self.cases[0][0].pos_start
+        self.pos_end = (self.else_case or self.cases[len(self.cases) - 1][0]).pos_end
+
 # PARSE RESULT
 
 class ParseResult:
@@ -422,6 +434,11 @@ class Parser:
                     "Expected ')'"
                 ))
 
+        elif tok.matches(TT_KEYWORD, "if"):
+            if_expr = res.register(self.if_expr())
+            if res.error: return res
+            return res.success(if_expr)
+
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
             "Expected int, float, identifier, '+', '-', '('"
@@ -445,6 +462,95 @@ class Parser:
 
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def if_expr(self):
+        res = ParseResult()
+        cases = []
+        else_case = None
+
+        if not self.current_tok.matches(TT_KEYWORD, "if"):
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected 'if'"
+            ))
+        res.register_advancement()
+        self.advance()
+
+        condition = res.register(self.expr())
+        if res.error: return res
+
+        if not self.current_tok.type == TT_LBRACE:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected '{'"
+            ))
+        res.register_advancement()
+        self.advance()
+
+        expr = res.register(self.expr())
+        if res.error: return res
+        cases.append((condition, expr))
+
+        if not self.current_tok.type == TT_RBRACE:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected '}'"
+            ))
+        res.register_advancement()
+        self.advance()
+
+        while self.current_tok.matches(TT_KEYWORD, "elif"):
+            res.register_advancement()
+            self.advance()
+
+            condition = res.register(self.expr())
+            if res.error: return res
+
+            if not self.current_tok.type == TT_LBRACE:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected '{'"
+            ))
+            res.register_advancement()
+            self.advance()
+
+            expr = res.register(self.expr())
+            if res.error: return res
+            cases.append((condition, expr))
+
+            if not self.current_tok.type == TT_RBRACE:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected '}'"
+                ))
+            res.register_advancement()
+            self.advance()
+
+        if self.current_tok.matches(TT_KEYWORD, "else"):
+            res.register_advancement()
+            self.advance()
+
+            if not self.current_tok.type == TT_LBRACE:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected '{'"
+            ))
+            res.register_advancement()
+            self.advance()
+
+            else_case = res.register(self.expr())
+            if res.error: return res
+
+            if not self.current_tok.type == TT_RBRACE:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected '}'"
+                ))
+            res.register_advancement()
+            self.advance()
+
+        return res.success(IfNode(cases, else_case))
+
 
     def comp_expr(self):
         res = ParseResult()
@@ -617,6 +723,9 @@ class Number:
         copy.set_context(self.context)
         return copy
 
+    def __true__(self):
+        return self.value != 0
+
     def __repr__(self):
         return str(self.value)
 
@@ -741,6 +850,25 @@ class Interpreter:
             return res.failure(error)
         else:
             return res.success(number.set_pos(node.pos_start, node.pos_end))
+
+    def visit_IfNode(self, node, context):
+        res = RTResult()
+
+        for condition, expr in node.cases:
+            condition_value = res.register(self.visit(condition, context))
+            if res.error: return res
+            
+            if condition_value.__true__():
+                expr_value = res.register(self.visit(expr, context))
+                if res.error: return res
+                return res.success(expr_value)
+
+        if node.else_case:
+            else_value = res.register(self.visit(node.else_case, context))
+            if res.error: return res
+            return res.success(else_value)
+
+        return res.success(None)
 
 # RUN
 
